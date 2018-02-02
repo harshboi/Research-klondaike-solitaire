89c04e9c5425e71feeda110c83c91c4ac687febf
Harshvardhan Singh <harshtix>
2018-02-01 23:12:47 -0800

    Completed tableau card setup functionality

--- solitaire.py
+++ solitaire.py
@@ -7,6 +7,7 @@ class game:
     #self.cards.update({"4":[['B','S'],['B','C'],['R','D'],['R','D']],"5":[['B','S'],['B','C'],['R','D'],['R','D']],"6":[['B','S'],['B','C'],['R','D'],['R','D']],"7":[['B','S'],['B','C'],['R','D'],['R','D']],"8":[['B','S'],['B','C'],['R','D'],['R','D']],"9":[['B','S'],['B','C'],['R','D'],['R','D']],"10":[['B','S'],['B','C'],['R','D'],['R','D']]})
     #self.cards.update({"A":[['B','S'],['B','C'],['R','D'],['R','D']]})
     self.cards = []
+    self.tcards = []
     self.turnstock = {}
     self.foundation = {}
     self.talon = {}
@@ -16,19 +17,20 @@ class game:
 
     n = []    
     for i in range(0,9):
-      n.append([1,'D','R',i+2])
-      n.append([1,'H','R',i+2])
-      n.append([1,'S','B',i+2])
-      n.append([1,'C','B',i+2])
+      n.append(['D','R',i+2])
+      n.append(['H','R',i+2])
+      n.append(['S','B',i+2])
+      n.append(['C','B',i+2])
     #n2 = n3 = n4 = n5 = n6 = n7 = n8 = n9 = n10 = [[1,'C','B'],[1,'D','R'],[1,'H','R'],[1,'S','B']]    #[[1,'c',b']] where 1 = no. of cards in the deck, c = club, b = black
     face_cards = ["K","Q","K","A"]
     for i in range(10,14):
-      n.append([1,'C','B',face_cards[i-10]])
-      n.append([1,'S','B',face_cards[i-10]])
-      n.append([1,'D','R',face_cards[i-10]])
-      n.append([1,'H','R',face_cards[i-10]])
+      n.append(['C','B',face_cards[i-10]])
+      n.append(['S','B',face_cards[i-10]])
+      n.append(['D','R',face_cards[i-10]])
+      n.append(['H','R',face_cards[i-10]])
     #for i in range(1,26):
     self.cards = n
+    self.tcards = n
     assign_tableau(n)
 
     #insert code
@@ -39,19 +41,17 @@ class game:
 # Paramterers: the list with all card combinations
 #########################################################################################################  
   def assign_tableau(self,n):
-    counter = 0
+    tableau_num = 0 
     
-    for i in range(0,7):      # For each tableau
-      counter = 1;            # Used to check if all the elements in n[i] have been occupied, iterates i to 4
-      
-      while(counter == 1):
-        x = random.randint(0,len(n))
-        if n[x][0] == 0:
-          continue
-        else if
-          append_tableau(x,i)
-          n[x][0] = 0
-          counter = 0
+    for i in range(0,28):      # For each tableau
+      x = random.randint(0,len(n))   # Contains the card number
+      card = n[x]                    # Stores a copy of the card
+      if(len(self.tableau(tableau_num)) < tableau_num+1):
+        self.tableau(tableau_num).append(card)
+        if(len(self.tableau(tableau_num)) == tableau_num+1)
+          tableau_num++
+      n.pop(x)
+    self.cards = n
 
 #########################################################################################################
 # Name: Used to add a card to the tabeau's seven card layer
